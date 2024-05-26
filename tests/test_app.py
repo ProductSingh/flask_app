31db2c5fa6ac033bdc2a8d46a3a5098091d1c6b6
Mankaran <mankaran43@gmail.com>
2024-05-26 14:02:40 +1000

    Fix import path for test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,10 +1,11 @@
-import sys
 import os
+import sys
 import pytest
-from app import app
 
 # Ensure the app module can be found
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from app import app
 
 @pytest.fixture
 def client():
